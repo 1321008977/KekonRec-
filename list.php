537c6c4e27683db1691d002c27d90796124eef12
1321008977 <63497854+1321008977@users.noreply.github.com>
2021-03-27 23:06:05 +0800

    Add files via upload

--- list.php
+++ list.php
@@ -4,9 +4,15 @@
 <?php
 $dir="/mnt/file/www/api.chinjufu.club/file/bilibili_Kekon_433494"; //指定的路径
 $sitepath = 'https://api.chinjufu.club/file/bilibili_Kekon_433494/';
+
 //遍历文件夹下所有文件
 if (false != ($handle = opendir ( $dir ))) {
-    echo " 录播：<BR/>暂时不考虑美化界面<BR/>可能会抽时间加入筛选，排序功能<BR/>";
+    echo " 录播：<BR/>录播顺序由时间排序</BR>";
+    echo " TODO: </BR>1.关键字搜索</BR>2.老录播转移</BR>3.优化UI</BR>";
+    echo " 已做: 在线播放没法获取视频长度的问题解决了</BR>";
+    echo "不管是谁在dos我，不得不说你有点";
+    echo "<h1 style='color:red'>菜</h1>";
+    echo "<img src='https://api.chinjufu.club/picture/low.jpg'/></BR>";
     $i = 0;
     $idnum = 0;
     while (false !== ($file = readdir($handle))) {
@@ -17,7 +23,9 @@ if (false != ($handle = opendir ( $dir ))) {
         //新建数组赋值文件name,time,size
         $fileinfo[$idnum]['name'] = $file;
         $fileinfo[$idnum]['time'] = date("Y-m-d H:i:s",filemtime($path));
-        $fileinfo[$idnum]['size'] = round((filesize($path)/1024),2);
+        $fileinfo[$idnum]['size'] = round((filesize($path)/1024/1024),2);
+        $fileinfo[$idnum]['url'] = $sitepath . $file;
+        $fileinfo[$idnum]['playerurl'] = 'https://api.chinjufu.club/player.php?pathurl='.$sitepath.$file;
         $filesize =  round((filesize($path)/1024),2);//获取文件大小              
         $filename = $path;//获取文件名称                                   
         $filetime = date("Y-m-d H:i:s",filemtime($path));//获取文件最近修改日期
@@ -41,10 +49,63 @@ if (false != ($handle = opendir ( $dir ))) {
     array_multisort($time,SORT_DESC,SORT_STRING, $fileinfo);//按时间排序
     //array_multisort($name,SORT_DESC,SORT_STRING, $fileinfo);//按名字排序
     //array_multisort($size,SORT_DESC,SORT_NUMERIC, $fileinfo);//按大小排序
-	foreach($fileinfo as $k=>$v){ 
-		foreach($fileinfo as $key=>$value){ 
-			echo '<a href="' . $sitepath . $file . '"id="'.$idnum.'">'.$file.'    '.'大小'.$fileinfo[$k]['size'].'  MiB':</a><br/>';
-		}
+	foreach($fileinfo as $k=>$v){
+			echo '<a href="' . $fileinfo[$k]['url'] . '"id="'.$k.'">' . $fileinfo[$k]['name']  . '</a>';
+			echo '大小 '.$fileinfo[$k]["size"].'MiB   ';
+			echo '修改时间 '.$fileinfo[$k]["time"];
+			echo '<a href="' . $fileinfo[$k]['playerurl'] . '"><button>在线播放</button></a> </br>';
+	}
+}
+?>
+	
+<?php
+$dir="/mnt/file/www/api.chinjufu.club/file2/bilibili_Kekon_433494"; //指定的路径
+$sitepath = 'https://api.chinjufu.club/file2/bilibili_Kekon_433494/';
+
+//遍历文件夹下所有文件
+if (false != ($handle = opendir ( $dir ))) {
+    echo " 冷数据</BR>";
+    $i = 0;
+    $idnum = 0;
+    while (false !== ($file = readdir($handle))) {
+        
+        if ($file != "." && $file != ".." && !is_dir($dir.'/'.$file)) {
+        	
+        $path = $dir.'/'.$file;
+        //新建数组赋值文件name,time,size
+        $fileinfo[$idnum]['name'] = $file;
+        $fileinfo[$idnum]['time'] = date("Y-m-d H:i:s",filemtime($path));
+        $fileinfo[$idnum]['size'] = round((filesize($path)/1024/1024),2);
+        $fileinfo[$idnum]['url'] = $sitepath . $file;
+        $fileinfo[$idnum]['playerurl'] = 'https://api.chinjufu.club/player.php?pathurl='.$sitepath.$file;
+        $filesize =  round((filesize($path)/1024),2);//获取文件大小              
+        $filename = $path;//获取文件名称                                   
+        $filetime = date("Y-m-d H:i:s",filemtime($path));//获取文件最近修改日期
+		       
+        $return =  $url.'/'.$file;
+        
+        
+            
+        }
+        $idnum = $idnum + 1;
+    }
+    //关闭句柄
+    closedir($handle);
+    
+    foreach($fileinfo as $k=>$v){
+        $size[$k] = $v['size'];
+        $time[$k] = $v['time'];
+        $name[$k] = $v['name'];
+    }
+    
+    array_multisort($time,SORT_DESC,SORT_STRING, $fileinfo);//按时间排序
+    //array_multisort($name,SORT_DESC,SORT_STRING, $fileinfo);//按名字排序
+    //array_multisort($size,SORT_DESC,SORT_NUMERIC, $fileinfo);//按大小排序
+	foreach($fileinfo as $k=>$v){
+			echo '<a href="' . $fileinfo[$k]['url'] . '"id="'.$k.'">' . $fileinfo[$k]['name']  . '</a>';
+			echo '大小 '.$fileinfo[$k]["size"].'MiB   ';
+			echo '修改时间 '.$fileinfo[$k]["time"];
+			echo '<a href="' . $fileinfo[$k]['playerurl'] . '"><button>在线播放</button></a> </br>';
 	}
 }
 ?>
